c21f751d5233d1f18ecddc2f455505592d3a25e3
Andrew Zhu <zhua15@mcmaster.ca>
2022-11-07 19:15:00 -0500

    started cart.tsx

--- src/Components/Cart.tsx
+++ src/Components/Cart.tsx
@@ -1,1 +1,31 @@
-import App from '../App';
+import React from 'react';
+import { Button } from '@mui/material';
+
+interface cartProps {
+    name: string;
+    price: number;
+    quantity: number;
+    options: optionsProps;
+}
+
+interface optionsProps {
+
+}
+
+const props = {
+    name: "pizza",
+	price: 10,
+	quantity: 2,
+	options: {}
+}
+
+const cart = (props: cartProps) => {
+    return (
+       <div>
+          <h1>Menu</h1>
+           <p>Menu page body content</p>
+       </div>
+    );
+}
+
+export default cart;
--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,1 +1,1 @@
-import App from '../App';
+import React from 'react';
