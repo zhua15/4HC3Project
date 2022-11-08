9ffd435284b5b73f06672ee6bba1afef2de2478b
Andrew Zhu <zhua15@mcmaster.ca>
2022-11-08 15:44:44 -0500

    Updates

---
+++ precache-manifest.26ce5fd44ad3a9e05dbf934bae12de9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa035dda658d646234",
+    "url": "./static/css/main.81e5abbd.chunk.css"
+  },
+  {
+    "revision": "72aa035dda658d646234",
+    "url": "./static/js/main.72aa035d.chunk.js"
+  },
+  {
+    "revision": "7fdc6048b962fd5f101d",
+    "url": "./static/js/1.7fdc6048.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "8b5ec82001f793a3f6aa0732b9ff836b",
+    "url": "./index.html"
+  }
+];
