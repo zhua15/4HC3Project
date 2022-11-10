b4e47650c2d8c9958cf4b29cb67b2fb42ebf4b02
Gilbert Cherrie <gills@LAPTOP-GKLG5HRR.localdomain>
2022-11-09 20:32:41 -0500

    Added dialog code

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Popup, { custimizationOptionProps, custimizationOptionsList, customizatio
 import Summary from './Pages/SummaryPage';
 import Cart, { cartProps, optionsProps } from './Components/Cart'
 
-const cart : cartProps[] = [
+const cart: cartProps[] = [
   {
     name: "pizza",
     price: 10,
@@ -20,49 +20,82 @@ const cart : cartProps[] = [
 ]
 
 const customizationOptions: custimizationOptionProps[] = [
-  {componentType: customizationType.single,
-  label: "Select Cheese",
-  options: [
-    {
-      optionName: "Mozerella"
-    } as custimizationOptionsList
-    ,
-    {
-      optionName: "Cheddar"
-    } as custimizationOptionsList,
-  ] as custimizationOptionsList[]
-}
+  {
+    componentType: customizationType.single,
+    label: "Select Cheese",
+    summaryViewLabel: "Cheese",
+    options: [
+      {
+        optionName: "Mozerella"
+      } as custimizationOptionsList
+      ,
+      {
+        optionName: "Cheddar"
+      } as custimizationOptionsList,
+      {
+        optionName: "Special Cheese",
+        price: 2.99
+      } as custimizationOptionsList
+    ] as custimizationOptionsList[]
+  },
+  {
+    componentType: customizationType.multi,
+    label: "Select Toppings",
+    summaryViewLabel: "Toppings",
+    options: [
+      {
+        optionName: "Bacon",
+        price: 1.99
+      } as custimizationOptionsList,
+      {
+        optionName: "Pepporoni"
+      } as custimizationOptionsList,
+      {
+        optionName: "Mushrooms"
+      } as custimizationOptionsList,
+      {
+        optionName: "Green Peppers"
+      } as custimizationOptionsList,
+      {
+        optionName: "Onions"
+      } as custimizationOptionsList,
+      {
+        optionName: "Yellow Peppers"
+      } as custimizationOptionsList
+    ]
+  }
 ]
 
-const popup : popupProps = {
-  name: "Pizza",
-  price: 8.99,
-  image: "./Images/testImage.png",
-  rating: 4,
-  ingrediants: [
-    "Cheese", 
-    "Dough", 
-    "Tomato Sauce"
-  ],
-  calories: 500,
-  customizationOptions: customizationOptions,
-} 
- 
+// const popup: popupProps = {
+//   setOpen: setSelectedItem,
+//   name: "Pizza",
+//   price: 8.99,
+//   image: "/images/pizza.jpg",
+//   rating: 4,
+//   ingrediants: [
+//     "Cheese",
+//     "Dough",
+//     "Tomato Sauce"
+//   ],
+//   calories: 500,
+//   customizationOptions: customizationOptions,
+// }
+
 class App extends Component {
   render() {
-    return (      
-       <HashRouter>
+    return (
+      <HashRouter>
         <div>
           <Routes>
-            <Route path="/" element={<Menu/>}/>
-            <Route path="/popup" element={<Popup/>}/>
-            <Route path="/summary" element={<Summary/>}/>
-            <Route path="/cart" element={<Cart {...cart}/>}/>
+            <Route path="/" element={<Menu />} />
+            {/* <Route path="/popup" element={<Popup {...popup} />} /> */}
+            <Route path="/summary" element={<Summary />} />
+            <Route path="/cart" element={<Cart {...cart} />} />
           </Routes>
-        </div> 
+        </div>
       </HashRouter>
     );
   }
 }
- 
+
 export default App;
--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,12 +1,89 @@
 import React from 'react';
- 
+import Button from '@mui/material/Button';
+import Popup, { custimizationOptionProps, custimizationOptionsList, customizationType, popupProps } from './PopupPage';
+
+const customizationOptions: custimizationOptionProps[] = [
+    {
+        componentType: customizationType.single,
+        label: "Select Cheese",
+        summaryViewLabel: "Cheese",
+        options: [
+            {
+                optionName: "Mozerella"
+            } as custimizationOptionsList
+            ,
+            {
+                optionName: "Cheddar"
+            } as custimizationOptionsList,
+            {
+                optionName: "Special Cheese",
+                price: 2.99
+            } as custimizationOptionsList
+        ] as custimizationOptionsList[]
+    },
+    {
+        componentType: customizationType.multi,
+        label: "Select Toppings",
+        summaryViewLabel: "Toppings",
+        options: [
+            {
+                optionName: "Bacon",
+                price: 1.99
+            } as custimizationOptionsList,
+            {
+                optionName: "Pepporoni"
+            } as custimizationOptionsList,
+            {
+                optionName: "Mushrooms"
+            } as custimizationOptionsList,
+            {
+                optionName: "Green Peppers"
+            } as custimizationOptionsList,
+            {
+                optionName: "Onions"
+            } as custimizationOptionsList,
+            {
+                optionName: "Yellow Peppers"
+            } as custimizationOptionsList
+        ]
+    }
+]
+
 const page = () => {
+    const [open, setOpen] = React.useState(false);
+    const [selectedItem, setSelectedItem] = React.useState({} as popupProps);
+
+    // Need to add this function to onClick of all menu cards and pass the card values to setSelectedItem.
+    // Card values should be of type popupProps and each menu card should have these values created
+    const handleClickOpen = () => {
+        setOpen(true);
+        const popup: popupProps = {
+            open: true,
+            setOpen: setOpen,
+            name: "Pizza",
+            price: 8.99,
+            image: "/images/pizza.jpg",
+            rating: 4,
+            ingrediants: [
+                "Cheese",
+                "Dough",
+                "Tomato Sauce"
+            ],
+            calories: 500,
+            customizationOptions: customizationOptions,
+        }
+        setSelectedItem(popup);
+    };
+
+    //Button can be removed but need to add an onClick function to all the menu cards, keep the line to render the popup as well
     return (
-       <div>
-          <h1>Menu</h1>
-           <p>Menu page body content</p>
-       </div>
+        <div>
+            <Button onClick={handleClickOpen}>scroll=paper</Button>
+            {open ? <Popup {...selectedItem} /> : null}
+            <h1>Menu</h1>
+            <p>Menu page body content</p>
+        </div>
     );
 }
- 
+
 export default page;
