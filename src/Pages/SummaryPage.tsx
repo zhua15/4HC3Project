8a80ea7674869aabea29edbc011a33e6a7d4f5a8
JiwooL0920 <JiwooL0920@gmail.com>
2022-11-15 23:04:51 -0500

    summary page change table to collapsible to show options

--- src/Pages/SummaryPage.tsx
+++ src/Pages/SummaryPage.tsx
@@ -15,9 +15,12 @@ import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import Collapse from '@mui/material/Collapse';
 import Typography from '@mui/material/Typography';
-
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+import { custimizationOptionProps, custimizationOptionsList, customizationType } from './PopupPage';
+import { Tab } from 'react-bootstrap';
+
 
 // faking props data 
 export interface orderHistoryProps {
@@ -28,31 +31,54 @@ export interface order {
    name: string;
    price: number;
    quantity: number;
-   options?: optionsProps;
+   options?: custimizationOptionProps[];
 }
 
-export interface optionsProps {
-
-}
 
 const fakeProps : order[] = [
    {
       name: "burger",
       price: 8, 
       quantity: 1,
-      options: []
    },
    {
       name: "pizza",
       price: 14, 
       quantity: 2,
-      options: []
+      options: [
+        {
+            componentType: 'Single',
+            label: "Select Cheese",
+            summaryViewLabel: "Cheese",
+            options: [
+                {
+                    optionName: "Mozerella"
+                } 
+            ] 
+        },
+        {
+            componentType: 'Multi',
+            label: "Select Toppings",
+            summaryViewLabel: "Toppings",
+            options: [
+                {
+                    optionName: "Bacon",
+                    price: 1.99
+                },
+                {
+                    optionName: "Pepporoni"
+                },
+                {
+                    optionName: "Yellow Peppers"
+                }
+            ]
+        }
+    ]
    },
    {
       name: "pasta",
       price: 10, 
       quantity: 5,
-      options: []
    },
 ]
 
@@ -63,61 +89,44 @@ const page = () => {
    const priceSum = fakeProps.reduce((sum, order) => sum + (order.price * order.quantity), 0)
    const tax = (priceSum * 0.13)
    const totalPrice = priceSum + tax + (priceSum * tip)
-    
-    // const DenseTable = () => {
-    //   return (
-    //     <TableContainer component={Paper}>
-    //       <Table sx={{ minWidth: 650 }} aria-label="a dense table">
-    //         <TableHead>
-    //           <TableRow
-    //           sx={{
-    //            fontWeight: 400
-    //           }}>
-    //             <TableCell><b>Name</b></TableCell>
-    //             <TableCell align="right"><b>Price</b></TableCell>
-    //             <TableCell align="right"><b>Quantity</b></TableCell>
-    //             <TableCell align="right"><b>Options</b></TableCell>
-    //             <TableCell align="right"><b>Total</b></TableCell>
-    //           </TableRow>
-    //         </TableHead>
-    //         <TableBody>
-    //           {fakeProps.map((row) => (
-    //             <TableRow
-    //               key={row.name}
-    //               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-    //             >
-    //               <TableCell component="th" scope="row">
-    //                 {row.name}
-    //               </TableCell>
-    //               <TableCell align="right">${row.price}</TableCell>
-    //               <TableCell align="right">{row.quantity}</TableCell>
-    //               <TableCell align="right">TODO</TableCell>
-    //               <TableCell align="right">${row.price * row.quantity}</TableCell>
 
-    //             </TableRow>
-    //           ))}
-    //         </TableBody>
-    //       </Table>
-    //     </TableContainer>
-    //   );
-    // }
-  
 
     const Row = (props: {row: order}) => {
       const { row } = props;
       const [open, setOpen] = React.useState(false);
       
+        
+      const RenderOptions = (props: {op: custimizationOptionProps}) => {
+        const {op} = props;
+        return (
+          <TableBody>
+            {op.options.map((opp:custimizationOptionsList) => (
+              <TableRow>
+                <TableCell align="left">{opp.optionName}</TableCell>
+                <TableCell align="right">{opp.price ? '$' + opp.price : ''}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+
+        )
+      }
+
       return (
+        
         <React.Fragment>
           <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
             <TableCell>
-              <IconButton
+              { row.options !== undefined ?
+                <IconButton
                 aria-label="expand row"
                 size="small"
                 onClick={() => setOpen(!open)}
-              >
+                >
                 {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-              </IconButton>
+                </IconButton>
+                : <></>
+              }
+             
             </TableCell>
             <TableCell component="th" scope="row">
               {row.name}
@@ -130,39 +139,16 @@ const page = () => {
           <TableRow>
             <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
               <Collapse in={open} timeout="auto" unmountOnExit>
-                <Box sx={{ margin: 1 }}>
-                  <Typography variant="h6" gutterBottom component="div">
-                    Options
-                  </Typography>
-                  <Table size="small" aria-label="purchases">
-                    <TableHead>
-                      <TableRow>
-                        {/* <TableCell>Description</TableCell>
-                        <TableCell>Price</TableCell> */}
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {/* {row.options &&
-                      row.options.map((option:any) => (
-                        <TableRow key={1}>
-                          <TableCell component="th" scope="row">
-                            D
-                          </TableCell>
-                          <TableCell>C</TableCell>
-                          <TableCell align="right">B</TableCell>
-                          <TableCell align="right">
-                            A
-                          </TableCell>
-                        </TableRow>
-                      ))} */}
-                      <TableRow key={1}>
-                          TODO
-                          {/* <TableCell component="th" scope="row">
-                            TODO
-                          </TableCell>
-                          <TableCell>Option Price</TableCell> */}
-                        </TableRow>
-                    </TableBody>
+                <Box ml="60%">
+                  <Table size="small" aria-label="purchases" >
+                  <TableRow>
+                    <TableCell align="left">Chosen Options</TableCell>
+                    <TableCell align="right">Additional Charge</TableCell>
+                  </TableRow>
+                    {(row.options === undefined) ? <></> :
+                     row.options.map((optionRow) => (
+                      <RenderOptions op={optionRow}/>
+                    ))}
                   </Table>
                 </Box>
               </Collapse>
@@ -172,43 +158,6 @@ const page = () => {
       );
     }
     
-    
-    const Subtotal = () => {
-       return (
-         <TableContainer component={Paper}>
-           <Table sx={{ minWidth: 700 }} aria-label="spanning table">
-              <TableBody>
-               <TableRow>
-                 <TableCell rowSpan={3} />
-                 <TableCell colSpan={2}>Subtotal</TableCell>
-                 <TableCell align="right">${priceSum}</TableCell>
-               </TableRow>
-               <TableRow>
-                 <TableCell>Tax</TableCell>
-                 <TableCell align="right">{"13%"}</TableCell>
-                 <TableCell align="right">${tax}</TableCell>
-               </TableRow>
-               <TableRow>
-                 <TableCell>Tip</TableCell>
-                 <TableCell align="right">
-                  <ButtonGroup variant="outlined">
-                        <Button onClick={() => setTip(0)}>0%</Button>
-                        <Button onClick={() => setTip(0.13)}>13%</Button>
-                        <Button onClick={() => setTip(0.20)}>20%</Button>
-                     </ButtonGroup>
-                 </TableCell>
-                 <TableCell align="right">${tip * priceSum}</TableCell>
-               </TableRow>
-               <TableRow>
-                 <TableCell colSpan={2}><b>Total</b></TableCell>
-                 <TableCell align="right"><b>${totalPrice.toFixed(2)}</b></TableCell>
-               </TableRow>
-             </TableBody>
-           </Table>
-         </TableContainer>
-       );
-     }
-
      const CollapsibleTable = () => {
       return (
         <TableContainer component={Paper}>
@@ -233,12 +182,48 @@ const page = () => {
       );
     }
 
+    const Subtotal = () => {
+      return (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 700 }} aria-label="spanning table">
+             <TableBody>
+              <TableRow>
+                <TableCell rowSpan={3} />
+                <TableCell colSpan={2}>Subtotal</TableCell>
+                <TableCell align="right">${priceSum}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Tax</TableCell>
+                <TableCell align="right">{"13%"}</TableCell>
+                <TableCell align="right">${tax}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Tip</TableCell>
+                <TableCell align="right">
+                 <ButtonGroup variant="outlined">
+                       <Button onClick={() => setTip(0)}>0%</Button>
+                       <Button onClick={() => setTip(0.13)}>13%</Button>
+                       <Button onClick={() => setTip(0.20)}>20%</Button>
+                    </ButtonGroup>
+                </TableCell>
+                <TableCell align="right">${tip * priceSum}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell colSpan={2}>Total</TableCell>
+                <TableCell align="right">${totalPrice.toFixed(2)}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      );
+    }
+
+
 
     return (
          <Box mx={10} my={3}>
             <h1>Payment</h1>
             <h2>Order History</h2>
-            {/* <DenseTable/> */}
             <CollapsibleTable/>
             
             <h2>Summary</h2>
