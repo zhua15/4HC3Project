75905fec74fa05fa0707262a3d7a8e5e0ec4461d
JiwooL0920 <JiwooL0920@gmail.com>
2022-11-15 19:45:36 -0500

    summary page use mui table

--- src/Pages/SummaryPage.tsx
+++ src/Pages/SummaryPage.tsx
@@ -1,8 +1,16 @@
 import { render } from '@testing-library/react';
 import React, { useEffect } from 'react';
 import { useState } from 'react'; 
-import OrderHistoryCard from '../Components/OrderHistoryCard';
-
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Box from '@mui/material/Box';
 
 // faking props data 
 export interface orderHistoryProps {
@@ -42,46 +50,127 @@ const fakeProps : order[] = [
 ]
 
 
-
-
 // summary page component
 const page = () => {
-   const [tip, setTip] = useState(0.13)
+   const [tip, setTip] = useState(0)
    const priceSum = fakeProps.reduce((sum, order) => sum + (order.price * order.quantity), 0)
    const tax = (priceSum * 0.13)
    const totalPrice = priceSum + tax + (priceSum * tip)
 
+   const createData = (
+      name: string,
+      calories: number,
+      fat: number,
+      carbs: number,
+      protein: number,
+    ) => {
+      return { name, calories, fat, carbs, protein };
+    }
+    
+    const rows = [
+      createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+      createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+      createData('Eclair', 262, 16.0, 24, 6.0),
+      createData('Cupcake', 305, 3.7, 67, 4.3),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+    ];
+    
+    const DenseTable = () => {
+      return (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="a dense table">
+            <TableHead>
+              <TableRow
+              sx={{
+               fontWeight: 400
+              }}>
+                <TableCell><b>Name</b></TableCell>
+                <TableCell align="right"><b>Price</b></TableCell>
+                <TableCell align="right"><b>Quantity</b></TableCell>
+                <TableCell align="right"><b>Options</b></TableCell>
+                <TableCell align="right"><b>Total</b></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {fakeProps.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">${row.price}</TableCell>
+                  <TableCell align="right">{row.quantity}</TableCell>
+                  <TableCell align="right">TODO</TableCell>
+                  <TableCell align="right">${row.price * row.quantity}</TableCell>
+
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      );
+    }
+    
+    
+    
+    const Subtotal = () => {
+       return (
+         <TableContainer component={Paper}>
+           <Table sx={{ minWidth: 700 }} aria-label="spanning table">
+              <TableBody>
+               <TableRow>
+                 <TableCell rowSpan={3} />
+                 <TableCell colSpan={2}>Subtotal</TableCell>
+                 <TableCell align="right">${priceSum}</TableCell>
+               </TableRow>
+               <TableRow>
+                 <TableCell>Tax</TableCell>
+                 <TableCell align="right">{"13%"}</TableCell>
+                 <TableCell align="right">${tax}</TableCell>
+               </TableRow>
+               <TableRow>
+                 <TableCell>Tip</TableCell>
+                 <TableCell align="right">
+                  <ButtonGroup variant="outlined">
+                        <Button onClick={() => setTip(0)}>0%</Button>
+                        <Button onClick={() => setTip(0.13)}>13%</Button>
+                        <Button onClick={() => setTip(0.20)}>20%</Button>
+                     </ButtonGroup>
+                 </TableCell>
+                 <TableCell align="right">${tip * priceSum}</TableCell>
+               </TableRow>
+               <TableRow>
+                 <TableCell colSpan={2}><b>Total</b></TableCell>
+                 <TableCell align="right"><b>${totalPrice.toFixed(2)}</b></TableCell>
+               </TableRow>
+             </TableBody>
+           </Table>
+         </TableContainer>
+       );
+     }
+
+
     return (
-       <div>
-          <h1>Summary</h1>
-          <h2>Order History</h2>
-          {fakeProps.map((order) => {
-            return (
-               <OrderHistoryCard
-                  name={order.name}
-                  price={order.price}
-                  quantity={order.quantity}
-                  options={order.options}
-               />
-            )
-          })}
-
-          <h2>Total Price</h2>
-          <p>Price: ${priceSum}</p>
-          <p>Tax: ${tax}</p>
-          <button onClick={() => setTip(0)}>0%</button>
-          <button onClick={() => setTip(0.13)}>13%</button>
-          <button onClick={() => setTip(0.20)}>20%</button>
-          <p>Tip: ${tip * priceSum} ({tip})</p>
-          <p>Total: ${totalPrice}</p>
-
-          <button>Call Service</button>
-          <button>Proceed to Payment</button>
-
-       </div>
+         <Box mx={10} my={3}>
+            <h1>Payment</h1>
+            <h2>Order History</h2>
+            <DenseTable/>
+            
+            <h2>Summary</h2>
+            <Subtotal/>
+         
+            <Box my={5} textAlign='right'>
+               <Button variant="outlined">Call Service</Button>
+               <Button variant="outlined">Proceed to Payment</Button>
+            </Box>
+         </Box>
     );
 }
  
 export default page;
+
+
 
 
