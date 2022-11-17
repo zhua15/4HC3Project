ed4777b8bb8c66f5e148e5611ef8ec6e9ded76f5
Andrew Zhu <zhua15@mcmaster.ca>
2022-11-17 17:34:18 -0500

    made cart look nicer

--- src/Components/Cart.tsx
+++ src/Components/Cart.tsx
@@ -3,12 +3,25 @@ import { Global } from '@emotion/react';
 import { styled } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { grey } from '@mui/material/colors';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { useNavigate } from "react-router-dom";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Box from '@mui/material/Box';
+
+import IconButton from '@mui/material/IconButton';
+import Collapse from '@mui/material/Collapse';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 export interface cartProps {
     cart: itemProps[],
@@ -75,6 +88,104 @@ const cart = (props: cartProps) => {
         }
     }
 
+    const Row = (props: {item: itemProps}) => {
+        const { item } = props;
+        const [open, setOpen] = React.useState(false);
+        
+        const getOptionsubtotal = (item: itemProps) => {
+          var optionPrice = 0
+          if (item.options) {
+            item.options.map((option) => {
+              optionPrice += option.price
+            })
+          }
+          return optionPrice
+        }   
+  
+        return (
+          <React.Fragment>
+            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+              <TableCell>
+                { item.options && item.options.length > 0 ?
+                  <IconButton
+                  aria-label="expand row"
+                  size="small"
+                  onClick={() => setOpen(!open)}
+                  >
+                  {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                  </IconButton>
+                  : <></>
+                }
+               
+              </TableCell>
+              <TableCell component="th" scope="row">{item.name}</TableCell>
+              <TableCell align="right">${item.price}</TableCell>
+              <TableCell align="right">
+                {(item.options && getOptionsubtotal(item) > 0)
+                  ? "$" + getOptionsubtotal(item) 
+                  : ""}
+              </TableCell>
+              <TableCell align="right">{item.quantity}</TableCell>
+              <TableCell align="right">${
+              item.options !== undefined 
+                ? ((item.price + getOptionsubtotal(item) )* item.quantity).toFixed(2)
+                : (item.price * item.quantity)
+              }
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                  <Box ml="61%" mr={3}>
+                    <Table size="small" aria-label="purchases" >
+                    <TableRow>
+                    </TableRow>
+                      {(item.options === undefined) 
+                        ? <></> 
+                        : // render each option name and price in condensed table
+                        <TableBody>
+                          {item.options &&
+                          item.options.map((option:optionsProps) => (
+                            <TableRow>
+                              <TableCell align="left">{option.name}</TableCell>
+                              <TableCell align="right">
+                                {option.price == 0 ? '' : '+ $' + option.price}
+                              </TableCell>
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      }
+                    </Table>
+                  </Box>
+                </Collapse>
+              </TableCell>
+            </TableRow>
+          </React.Fragment>
+        );
+      }
+
+    const CollapsibleTable = () => {
+        return (
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="cart table">
+              <TableHead>
+                <TableRow>
+                  <TableCell><b>Details</b></TableCell>
+                  <TableCell><b>Name</b></TableCell>
+                  <TableCell align="right"><b>Price</b></TableCell>
+                  <TableCell align="right"><b>Options</b></TableCell>
+                  <TableCell align="right"><b>Quantity</b></TableCell>
+                  <TableCell align="right"><b>Total</b></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {props.cart.map((item) => ( <Row key={item.name} item={item} /> ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        );
+      }
+
     return (
         <Root>
             <CssBaseline />
@@ -101,7 +212,7 @@ const cart = (props: cartProps) => {
                     sx={{
                         px: 2,
                         pb: 2,
-                        height: '100%',
+                        height: '40%',
                     }}
                 >
                     &nbsp;
@@ -123,60 +234,7 @@ const cart = (props: cartProps) => {
                         overflow: 'auto',
                     }}
                 >
-
-                    {props.cart.map((item: itemProps) => {
-                        return (
-                            <div>
-                                <Box textAlign='center'>
-                                    <div style={{
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        flexWrap: 'wrap',
-                                        columnGap: '50px',
-                                    }}>
-                                        <Button onClick={(a) => handleDelete(item)}>
-                                            <DeleteIcon />
-                                        </Button>
-
-                                        <Typography
-                                            align='center'>
-                                            name: {item.name}
-                                        </Typography>
-                                        <Typography
-                                            align='center'>
-                                            price: ${item.price}
-                                        </Typography>
-                                        <Typography
-                                            align='center'>
-                                            quantity: {item.quantity}
-                                        </Typography>
-                                    </div>
-
-                                    {item.options ? item.options.map((option: optionsProps) => {
-                                        return (
-                                            <div style={{
-                                                display: 'flex',
-                                                alignItems: 'center',
-                                                flexWrap: 'wrap',
-                                                columnGap: '50px',
-                                            }}>
-                                                <div />
-                                                <div />
-                                                <Typography
-                                                    align='center'>
-                                                    name: {option.name}
-                                                </Typography>
-                                                <Typography
-                                                    align='center'>
-                                                    price: ${option.price}
-                                                </Typography>
-                                            </div>
-                                        )
-                                    }) : null}
-                                </Box>
-                            </div>
-                        )
-                    })}
+                    <CollapsibleTable/>
                     <Typography
                         align='center'
                         variant='h6'
