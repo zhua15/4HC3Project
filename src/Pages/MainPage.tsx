2000127de51d7901818a10908bc49cce1a0b388c
Andrew Zhu <zhua15@mcmaster.ca>
2022-11-20 03:14:01 -0500

    i am in pain

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -10,6 +10,7 @@ import Typography from '@mui/material/Typography';
 import { ShoppingCart } from '@mui/icons-material';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import throttle from "lodash/throttle";
 
 interface TabPanelProps {
     children?: React.ReactNode;
@@ -83,6 +84,47 @@ function TabPanel(props: TabPanelProps) {
             }
         </div >
     );
+}
+
+const makeUnique: any = (hash: any, unique: any, i = 1) => {
+const uniqueHash = i === 1 ? hash : `${hash}-${i}`;
+
+if (!unique[uniqueHash]) {
+    unique[uniqueHash] = true;
+    return uniqueHash;
+}
+
+return makeUnique(hash, unique, i + 1);
+};
+
+const textToHash = (text: String, unique = {}) => {
+return makeUnique(
+    encodeURI(
+    text
+        .toLowerCase()
+        .replace(/=&gt;|&lt;| \/&gt;|<code>|<\/code>|&#39;/g, "")
+        // eslint-disable-next-line no-useless-escape
+        .replace(/[!@#\$%\^&\*\(\)=_\+\[\]{}`~;:'"\|,\.<>\/\?\s]+/g, "-")
+        .replace(/-+/g, "-")
+        .replace(/^-|-$/g, "")
+    ),
+    unique
+);
+};
+
+function useThrottledOnScroll(callback: any, delay: any) {
+const throttledCallback = React.useMemo(() => throttle(callback, delay), [
+    callback,
+    delay
+]);
+
+React.useEffect(() => {
+    window.addEventListener("scroll", throttledCallback);
+    return () => {
+    window.removeEventListener("scroll", throttledCallback);
+    throttledCallback.cancel();
+    };
+}, [throttledCallback]);
 }
 
 const page = (props: { orderHistoryProps: itemProps[] }) => {
@@ -187,12 +229,76 @@ const page = (props: { orderHistoryProps: itemProps[] }) => {
         setValue(newValue);
     };
 
+    const [activeState, setActiveState] = React.useState(null);
+  const  tabsInScroll  = [{text: "Mains", component: mains}, {text: "Snacks", component: Snacks}, 
+  {text: "Desserts", component: desserts}, {text: "Drinks", component: drinks}];
+
+  let itemsServer = tabsInScroll.map((tab) => {
+    const hash = textToHash(tab.text);
+    return {
+      text: tab.text,
+      component: tab.component,
+      hash: hash,
+      node: document.getElementById(hash)
+    };
+  });
+
+  const itemsClientRef: React.MutableRefObject<any[]> = React.useRef([]);
+  React.useEffect(() => {
+    itemsClientRef.current = itemsServer;
+  }, [itemsServer]);
+
+  const clickedRef = React.useRef(false);
+  const unsetClickedRef: React.MutableRefObject<any> = React.useRef(null);
+  const findActiveIndex = React.useCallback(() => {
+    // set default if activeState is null
+    if (activeState === null) setActiveState(itemsServer[0].hash);
+
+    // Don't set the active index based on scroll if a link was just clicked
+    if (clickedRef.current) return;
+
+    let active;
+    for (let i = itemsClientRef.current.length - 1; i >= 0; i -= 1) {
+      // No hash if we're near the top of the page
+      if (document.documentElement.scrollTop < 0) {
+        active = { hash: null };
+        break;
+      }
+
+      const item = itemsClientRef.current[i];
+
+      if (
+        item.node &&
+        item.node.offsetTop <
+          document.documentElement.scrollTop +
+            document.documentElement.clientHeight / 8 +
+            48
+      ) {
+        active = item;
+        break;
+      }
+    }
+
+    if (active && activeState !== active.hash) {
+      setActiveState(active.hash);
+    }
+  }, [activeState, itemsServer]);
+
+  // Corresponds to 10 frames at 60 Hz
+  useThrottledOnScroll(itemsServer.length > 0 ? findActiveIndex : null, 166);
+
+  React.useEffect(
+    () => () => {
+      clearTimeout(unsetClickedRef.current);
+    },
+    []
+  );
+
     //Button can be removed but need to add an onClick function to all the menu cards, keep the line to render the popup as well
     return (
         <div style={{ background: 'lightCyan' }}>
             <div>
                 <Box
-                    m={1}
                     borderBottom={1}
                     borderColor='divider'
                     display="flex"
@@ -202,40 +308,29 @@ const page = (props: { orderHistoryProps: itemProps[] }) => {
                     sx={{
                         position: "sticky",
                         top: 0,
+                        backgroundColor: 'lightcyan'
                     }}>
-                    <div>
-                        <Tabs variant="scrollable" scrollButtons="auto"
-                        value={value} onChange={handleChangeTabs} aria-label="basic tabs">
-                            <Tab sx={{ fontWeight: 'bold', color: 'black' }} label="Mains" {...a11yProps(0)} />
-                            <Tab sx={{ fontWeight: 'bold', color: 'black' }} label="Snacks" {...a11yProps(1)} />
-                            <Tab sx={{ fontWeight: 'bold', color: 'black' }} label="Desserts" {...a11yProps(2)} />
-                            <Tab sx={{ fontWeight: 'bold', color: 'black' }} label="Drinks" {...a11yProps(3)} />
-                        </Tabs>
-                    </div>
-                    <div style={{ paddingLeft: "2rem", paddingBottom: "0.5rem" }}>
+                        <Tabs value={activeState ? activeState : itemsServer[0].hash}>
+          {itemsServer.map((item2) => (
+            <Tab key={item2.hash} label={item2.text} value={item2.hash} />
+          ))}
+        </Tabs>
+        <div style={{ paddingLeft: "2rem", paddingBottom: "0.5rem" }}>
                         <Button variant='contained' onClick={toggleDrawer()}><ShoppingCart /></Button>
                     </div>
+        
+                    
+                    
                 </Box >
-                <TabPanel value={value} index={0}>
-                    <Grid container spacing={2} direction='row'>
-                        {mains}
+                <div>
+        {itemsServer.map((item1) => (
+          <div id={item1.hash} key={item1.text}>
+            <Grid container spacing={2} direction='row'>
+                        {item1.component}
                     </Grid>
-                </TabPanel>
-                <TabPanel value={value} index={1}>
-                    <Grid container spacing={2} direction='row'>
-                        {Snacks}
-                    </Grid>
-                </TabPanel>
-                <TabPanel value={value} index={2}>
-                    <Grid container spacing={2} direction='row'>
-                        {desserts}
-                    </Grid>
-                </TabPanel>
-                <TabPanel value={value} index={3}>
-                    <Grid container spacing={2} direction='row'>
-                        {drinks}
-                    </Grid>
-                </TabPanel>
+          </div>
+        ))}
+      </div>
                 {open ? <Popup {...selectedItem} /> : null}
                 <Cart cart={cart} setCart={setCart} bleeding={0} open={openCart} toggleDrawer={toggleDrawer} setHistory={setOrderHistory} />
             </div >
